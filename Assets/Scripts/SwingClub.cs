25754e476781e908920371f1efdb6f607133f4f8
Ben Lewis <ben.lewis@gmail.com>
2014-12-29 22:08:55 -0800

    swinging with input

--- Assets/Scripts/SwingClub.cs
+++ Assets/Scripts/SwingClub.cs
@@ -3,20 +3,31 @@ using System.Collections;
 
 public class SwingClub : MonoBehaviour {
 
-	public Rigidbody ball;
-	public float force = 100.0f;
-	public Camera mainCamera;
-	public Transform hitDirection;
-	
-	public float sleepVelocity = 0.5f;
-	public float sleepAngularVelocity = 3.5f;
-	
-	public float minSleepTime = 0.5f;
-	
+	public Transform ball;
+	public Transform hole;
+	public Transform club;
+	public float maxForce = 300.0f;
+				
+	/*
+	 *	Figure out when the ball has stopped moving
+	*/			
+	private float sleepVelocity = 0.5f;
+	private float sleepAngularVelocity = 3.5f;	
+	private float minSleepTime = 0.5f;	
 	private float hitTime;
 	private bool sleeping = false;
 	
-	private Vector3 cameraPositionToBall = new Vector3(0.0f, 1.0f, -0.75f);
+	/* 
+	 * 	After the ball sleeps we need to move the golfer
+	*/
+	private Vector3 golferPositionToBall = Vector3.up * 0.8f;
+	
+	/*
+	 * 	How long is the swing in motion
+	 * 	And how much force will be applied to the ball when we hit it
+	*/
+	private float force = 0.0f;
+	private float swingTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -34,28 +45,45 @@ public class SwingClub : MonoBehaviour {
 			PutBallToSleep();
 		}
 		
-		if (sleeping && Input.GetKeyDown (KeyCode.Space)) {
-			HitBall();
+		if (sleeping && Input.GetKey (KeyCode.Space)) {
+			SwingUp();
+		} else if (sleeping && swingTime > 0.0f) {
+			SwingDown();
 		}
 	}
 	
 	public void PutBallToSleep() {
 		ball.rigidbody.Sleep();
 		sleeping = true;
-		ball.transform.renderer.material.color = Color.green;
-		mainCamera.transform.position = ball.transform.position + cameraPositionToBall;
+		ball.renderer.material.color = Color.green;
 		
-		hitDirection.transform.position = ball.transform.position;
-		hitDirection.gameObject.SetActive(true);
+		transform.position = ball.position + golferPositionToBall;
+		club.localEulerAngles = Vector3.zero;
+		force = 0.0f;
+		swingTime = 0.0f;
+	}
+	
+	public void SwingUp() {
+		if (force < maxForce) {
+			swingTime += Time.deltaTime;
+			club.localEulerAngles += Vector3.right * 1.0f;
+			force += 5.0f;
+		}
+	}
+	
+	public void SwingDown() {
+		swingTime -= Time.deltaTime;
+		club.localEulerAngles -= Vector3.right * 1.0f;
+		if (swingTime <= 0.0f)
+			HitBall();
 	}
 	
 	public void HitBall() {
 		// Swing!
-		Vector3 forward = hitDirection.forward;
+		Vector3 forward = transform.forward;
 		forward.y = 0;
 		forward.Normalize();
-		hitDirection.gameObject.SetActive(false);
-		ball.AddForce(forward * force);
+		ball.rigidbody.AddForce(forward * force);
 		sleeping = false;
 		
 
