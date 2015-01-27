9066574f40d22caabd62a24cbe0a1a45511700d6
Ben Lewis <ben.lewis@gmail.com>
2015-01-26 20:21:11 -0800

    aim helper

--- Assets/Scripts/RotateInputController.cs
+++ Assets/Scripts/RotateInputController.cs
@@ -7,6 +7,7 @@ public class RotateInputController : MonoBehaviour {
 	public float degreesPerSecond = 90.0f;
 	public Transform hmd;
 	public MoveCamera monitor;
+	public Transform rayObject;
 	
 	private float lastY;
 	private Golfer golfer;
@@ -44,15 +45,36 @@ public class RotateInputController : MonoBehaviour {
 			Transform t = hmd;
 			if (t == null || !t.gameObject.activeInHierarchy)
 				t = monitor.transform;
-			if (t) {
-				Debug.Log ("Rotating club to face the direction of " + t.gameObject.name);
-				float y = t.localEulerAngles.y;
-				transform.localEulerAngles = new Vector3(
-					transform.localEulerAngles.x, 
-					y, 
-					transform.localEulerAngles.z
-				);			
+				
+			RaycastHit hitInfo;
+			if (t && Physics.Raycast(t.position, t.forward, out hitInfo)) {
+				rayObject.transform.position = hitInfo.point;
+				rayObject.renderer.enabled = true;
+				
+				//find the vector pointing from the ball position to the hole
+				Vector3 directionToFace = (hitInfo.point - transform.position).normalized;
+				
+				//create the rotation we need to be in to look at the hole
+				Quaternion bodyLookRotation = Quaternion.LookRotation(directionToFace);
+				
+				// Rotate the golfer to face the hole
+				transform.rotation = bodyLookRotation;
+				
+				// Don't aim up
+				transform.eulerAngles = Vector3.Scale (transform.eulerAngles, Vector3.up);
+				
+				
+//				float y = t.localEulerAngles.y;
+//				transform.localEulerAngles = new Vector3(
+//					transform.localEulerAngles.x, 
+//					y, 
+//					transform.localEulerAngles.z
+//				);		
+			} else {
+				rayObject.renderer.enabled = false;				
 			}			
+		} else {
+			rayObject.renderer.enabled = false;			
 		}
 		
 		float rotation = device.RightTrigger - device.LeftTrigger;
