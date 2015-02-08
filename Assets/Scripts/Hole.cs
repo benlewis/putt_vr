a01248f41ff989f1baa8aa3da0310a3ab641b045
Ben Lewis <ben.lewis@gmail.com>
2015-02-08 16:55:59 -0500

    hole markers and better pars

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -15,6 +15,13 @@ public class Hole : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		strokes = 0;
+		TextMesh[] holeTexts = GetComponentsInChildren<TextMesh>();
+		foreach (TextMesh text in holeTexts) {
+			switch(text.name) {
+				case "Name Text": text.text = name; break;
+				case "Par Text": text.text = string.Format("Par {0}", par); break;
+			}
+		}
 	}
 	
 	public void AddStroke() {
